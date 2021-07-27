2a8e2929f587d1ab26b055b0119967f547afe81c
CWhirl <68519564+CWhirl@users.noreply.github.com>
2021-07-27 04:04:06 -0600

    added view functions

--- db/schema.sql
+++ db/schema.sql
@@ -30,12 +30,3 @@ CREATE TABLE employee (
   FOREIGN KEY (manager_id) REFERENCES employee (id)
 );
 
-INSERT INTO departments (name)
-VALUES ('Manager'), ('Sales'), ('Engineer'), ('Accounting');
-
-INSERT INTO Roles (title, salary, department_id)
-VALUES ('Sales Manager', 200000, 1), ('Engineer', 300000, 2), ('Sales Associate', 150000, 3), ('Bookkeeper ', 600000, 5);
-
-INSERT INTO Employees (first_name, last_name, role_id, manager_id)
-VALUES ('Bill','Johnson', 1, NULL), ('Ricky', 'Goodman', 2, NULL), ('Gabe', 'Mora', 3, NULL), ('Robby', 'Mark', 4, NULL);
-
--- index.js
+++ index.js
@@ -1,52 +1,93 @@
-const { prompt } = require('inquirer');
-const DATABASE = require('./db')
-require('console.table');
+const mysql = require("mysql");
+const util = require("util");
 
-// use inquirer to prompt the user
-prompt([
-  {
-    type: 'list',
-    name: 'appFlow',
-    message: 'What would you like to do?',
-    choices: [
-        "View Employees",
-        "View Departments",
-        "View Jobs",
-        "Add Employee",
-        "Add Department",
-        "Add Job",
-        "Update Role",
-        "Exit"
-    ]
-  }
-], (answers) => {
-    switch (answers) {
-        case "View Employees":
-            console.log(`\n`);
-            console.table(DATABASE.viewEmployees());
-            break;
-        case "View Departments":
-            console.log(`\n`);
-            console.table(DATABASE.viewDepartments());
-            break;
-        case "View Jobs":
-            console.log(`\n`);
-            console.table(DATABASE.viewJobs());
-            break;
-        case "Add Employee":
-            DATABASE.addEmployee();
-            break;
-        case "Add Department":
-            DATABASE.addDepartment();
-            break;
-        case "Add Job":
-            DATABASE.addJob();
-            break;
-        case "Update Role":
-            DATABASE.updateRole();
-            break;
-        case "Exit":
-            process.exit();
-            break;
-    }
+var connString = `mysql://root:pass@localhost/employee_db?charset=utf8_general_ci&timezone=-0700`;
+
+const connection = mysql.createConnection(connString);
+
+connection.connect((err) => {
+  if (err) throw err;
+  console.log(`connected as id ${connection.threadId}\n`);
 });
+
+connection.query = util.promisify(connection.query);
+
+const inquirer = require("inquirer");
+require("console.table");
+
+async function init() {
+  // use inquirer to prompt the user
+  await inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "appFlow",
+        message: "What would you like to do?",
+        choices: [
+          "View Employees",
+          "View Departments",
+          "View Jobs",
+          "Add Employee",
+          "Add Department",
+          "Add Job",
+          "Update Role",
+          "Exit",
+        ],
+      },
+    ])
+    .then((answers) => {
+      console.log(answers);
+      switch (answers.appFlow) {
+        case "View Employees":
+          console.log(`\n`);
+          console.table(viewEmployees());
+          break;
+        case "View Departments":
+          console.log(`\n`);
+          console.table(viewDepartments());
+          break;
+        case "View Jobs":
+          console.log(`\n`);
+          console.table(viewJobs());
+          break;
+        case "Add Employee":
+          DATABASE.addEmployee();
+          break;
+        case "Add Department":
+          DATABASE.addDepartment();
+          break;
+        case "Add Job":
+          DATABASE.addJob();
+          break;
+        case "Update Role":
+          DATABASE.updateRole();
+          break;
+        case "Exit":
+          process.exit();
+          break;
+      }
+    });
+}
+
+init();
+
+function viewJobs() {
+  connection.query(`SELECT * FROM job ORDER BY id`, (err, answer) => {
+    if (err) throw err;
+    console.table("Jobs:", answer);
+  });
+}
+
+function viewDepartments() {
+  connection.query(`SELECT * FROM department ORDER BY id`, (err, answer) => {
+    if (err) throw err;
+    console.table("Departments:", answer);
+  });
+}
+
+function viewEmployees() {
+  connection.query(`SELECT * FROM employee ORDER BY id`, (err, answer) => {
+    if (err) throw err;
+    console.table("Employees:", answer);
+  });
+}
