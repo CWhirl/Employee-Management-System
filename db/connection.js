e0c3ee4f4a33df3bc26f52dda23f04a90da2d92d
CWhirl <68519564+CWhirl@users.noreply.github.com>
2021-07-27 01:39:44 -0600

    fixed env link

--- db/connection.js
+++ db/connection.js
@@ -4,8 +4,8 @@ const util = require('util');
 const connection = mysql.createConnection({
     host: process.env.DB_HOST,
     port: 3306,
-    user: 'root',
-    password: '',
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
     database: 'employee_db'
 });
 
